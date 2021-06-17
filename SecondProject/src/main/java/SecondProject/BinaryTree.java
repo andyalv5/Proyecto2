d3a3b1c1457007ca88b81a8c8c0fd7cd5a36f69a
Andy <andynet@iMac-de-Andy-alv.local>
2021-06-17 18:50:30 -0400

    empezado el arbol

---
+++ SecondProject/src/main/java/SecondProject/BinaryTree.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SecondProject;
+
+/**
+ *
+ * @author andynet
+ */
+
+//arbol binario
+public class BinaryTree{
+
+    private TreeNode pRoot;
+    
+    //Constructor arbol
+    public BinaryTree(){
+        
+    }
+    
+}
